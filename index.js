9f2aea3a2eeca5482b38aa6e5554f417d75716a1
Jeremy Robles <jeremy.robles56@gmail.com>
2016-08-14 16:24:03 -0700

    updated structure

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var alexa = require('alexa-app');
 var app = new alexa.app();
 
-var paragraph = [
-	request.session('name')+" is the only "+request.session('object')+" who is able to see the "+request.session('thing')+" of the "+request.session('subject')+".",
-	"The "+request.session('subject')+'is the '+request.session('object')+"of"+request.session('name')
-];
+// var harryPotterQuote = "When a (object) arrives for (adjective) but (adjective) (name) , a (number) year-old (noun) is revealed to him that apparently he's the last to know. His parents were wizards (noun), (verb) by a Dark Lord's (evil person) curse when (name) just a baby, and which he somehow (adverb) survived. Leaving his unsympathetic aunt and uncle for (place), a (verb – skill) school brimming with ghosts (noun) and enchantments, (name) stumbles upon a (adjective) adventure when he finds a three (number)-headed (animal) guarding a (noun – location in a home) on the third floor. Then he hears of a missing stone (noun) with (adjective)ing powers which could be (adjective), (adjective), or both.";
 
 /**
  * LaunchRequest.
@@ -24,6 +21,9 @@ app.launch(function(request,response) {
 		title:   "Madlib Skill",  //this is not required for type Simple 
 		content: "Welcome to the madlib skill"
 	});
+	response.shouldEndSession(false);
+	response.send();
+
 
 });
 app.intent('SetNameIntent', {
@@ -33,6 +33,7 @@ app.intent('SetNameIntent', {
 	response.session('name', name);
 	response.say('setting the name to '+name);
 	response.reprompt('what do you want to set the name to?');
+	response.shouldEndSession(false);
 	response.send();
 });
 app.intent('SetSubjectIntent', {
@@ -42,6 +43,7 @@ app.intent('SetSubjectIntent', {
 	response.session('subject', subject);
 	response.say('setting the subject to '+subject);
 	response.reprompt('what do you want to set the subject to?');
+	response.shouldEndSession(false);
 	response.send();
 });
 app.intent('SetObjectIntent', {
@@ -51,13 +53,18 @@ app.intent('SetObjectIntent', {
 	response.session('object', object);
 	response.say('setting the object to '+object);
 	response.reprompt('what do you want to set the object to?');
+	response.shouldEndSession(false);
 	response.send();
 });
 
 app.intent('ReadMadlibIntent', {
-
+	'utterances':[ 'read the paragraph' ]
 }, function(request, response){
 	if(request.session('name') != '' && request.session('subject') != '' && request.session('object')!= '' ){
+		var paragraph = [
+			request.session('name')+" is the only "+request.session('object')+" who is able to see the "+request.session('subject')+".",
+			"The "+request.session('subject')+'is the '+request.session('object')+"of"+request.session('name')
+		];
 		response.say(paragraph[Math.floor(Math.random()*paragraph.length)]);
 		response.shouldEndSession(true);
 		response.send();
@@ -66,18 +73,21 @@ app.intent('ReadMadlibIntent', {
 			response.say('It looks like you have not selected a persons name just yet. Set a name by saying ');
 			response.say('set the name, and the name you want to enter. ');
 			response.reprompt('set the name, and the name you want to enter ');
+			response.shouldEndSession(false);
 			response.send();
 		}
 		if(!request.session('object')){
 			response.say('It looks like you have not selected a object just yet. Set a object by saying ');
 			response.say('set the object, and the object you want to enter. ');
 			response.reprompt('set the object, and the object you want to enter ');
+			response.shouldEndSession(false);
 			response.send();
 		}
 		if(!request.session('subject')){
 			response.say('It looks like you have not selected a subject just yet. Set a subject by saying ');
 			response.say('set the subject, and the subject you want to enter. ');
 			response.reprompt('set the subject, and the subject you want to enter. ');
+			response.shouldEndSession(false);
 			response.send();
 		}
 	}
